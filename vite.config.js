0cbc267e22943d6869c9fadabc8311bf39f10cd4
Rémi Bovard <remi.bovard@vevey.ch>
2025-02-05 15:49:16 +0100

    Add env variable for application base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-export default {
-  base: '/luftbilder/', // for local use '',
-  build: {
-    sourcemap: true,
-  }
-}
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+  return {
+    base: env.VITE_BASE_PATH,
+    build: {
+      sourcemap: true,
+    }
+  };
+});
