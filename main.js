142ef01f42451266545d0493c51c0d246e452dbc
Rémi Bovard <remi.bovard@vevey.ch>
2025-02-07 15:19:25 +0100

    Refactoring

--- main.js
+++ main.js
@@ -11,31 +11,35 @@ import GeoTIFF from 'ol/source/GeoTIFF.js';
 import WMTSCapabilities from 'ol/format/WMTSCapabilities';
 import { optionsFromCapabilities } from 'ol/source/WMTS';
 
-const params = new URLSearchParams(document.location.search);
-const url = params.get('url');
-const east = parseFloat(params.get('east'));
-const north = parseFloat(params.get('north'));
-const img_type = params.get('type');
+const urlParams = new URLSearchParams(document.location.search);
+const imageUrl = urlParams.get('url');
+const east = parseFloat(urlParams.get('east'));
+const north = parseFloat(urlParams.get('north'));
+const imageType = urlParams.get('type');
 
-const img_type_tag = document.getElementById('image_type');
-img_type === 'ortho'
-  ? img_type_tag.innerText = 'Type d\'image: Orthophoto'
-  : img_type_tag.innerText = 'Type d\'image: Image aérienne';
+const imageTypeNames = {
+  ortho: 'Orthophoto',
+  aerial: 'Image aérienne',
+};
+const imageTypeText = imageTypeNames[imageType] || 'Inconnu';
+document.getElementById('image_type').innerText = `Type d'image: ${imageTypeText}`;
 
-const img = url.split('/').slice(-1);
-const h5 = document.getElementById('image_id');
-const aTag = document.createElement('a');
-aTag.href = url;
-aTag.innerHTML = img;
-aTag.setAttribute('target', '_blank');
-h5.appendChild(aTag);
+if (imageUrl) {
+  const imageFileName = imageUrl.split('/').pop();
+  const imageIdElement = document.getElementById('image_id');
+  const imageLink = document.createElement('a');
+  imageLink.href = imageUrl;
+  imageLink.innerText = imageFileName;
+  imageLink.setAttribute('target', '_blank');
+  imageIdElement.appendChild(imageLink);
+}
 
 const crs = 'EPSG:2056';
 const matrixSet = import.meta.env.VITE_WMTS_MATRIXSET;
 const capabilitiesUrl = import.meta.env.VITE_WMTS_CAPABILITIES_URL;
 const backgroundLayerName = import.meta.env.VITE_WMTS_BACKGROUND_LAYER_NAME;
 
-let imgLayer;
+let imageLayer;
 
 proj4.defs(crs,
   '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333'
@@ -43,9 +47,7 @@ proj4.defs(crs,
   + '+towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs');
 
 register(proj4);
-const projection = new Projection({
-  code: crs,
-});
+const projection = new Projection({ code: crs });
 
 fetch(capabilitiesUrl)
   .then(response => response.text())
@@ -64,29 +66,29 @@ fetch(capabilitiesUrl)
     const resolutions = wmtsSource.getResolutions();
     const matrixIds = resolutions.map((_, index) => index);
     const tileGrid = new WMTSTileGrid({
-        origin: [extent[0], extent[3]],
-        resolutions,
-        matrixIds,
+      origin: [extent[0], extent[3]],
+      resolutions,
+      matrixIds,
     });
 
     wmtsSource.tileGrid = tileGrid;
     wmtsSource.projection = projection;
 
     const backgroundLayer = new TileLayer({
-      source: wmtsSource
+      source: wmtsSource,
     });
 
-    imgLayer = new WebGLTileLayer({
+    imageLayer = new WebGLTileLayer({
       style: {
         color: [
           'case',
           ['<', ['*', ['band', 1], 255], 10],
           ['color', 255, 0, 0, 0],
-          ['color', ['*', ['band', 1], 255], 1]
+          ['color', ['*', ['band', 1], 255], 1],
         ],
       },
       source: new GeoTIFF({
-        sources: [{ url: url, nodata: NaN }]
+        sources: [{ url: imageUrl, nodata: NaN }],
       }),
     });
 
@@ -99,18 +101,20 @@ fetch(capabilitiesUrl)
     });
 
     new Map({
-      layers: [backgroundLayer, imgLayer],
+      layers: [backgroundLayer, imageLayer],
       target: 'sitn-map',
       view,
     });
   });
 
-const opacityInput = document.getElementById('opacity-input');
-const opacityOutput = document.getElementById('opacity-output');
+const opacitySlider = document.getElementById('opacity-input');
+const opacityLabel = document.getElementById('opacity-output');
 
-function update() {
-  const opacity = parseFloat(opacityInput.value);
-  imgLayer.setOpacity(opacity);
-  opacityOutput.innerText = opacity.toFixed(2);
+function updateOpacity() {
+  if (imageLayer) {
+    const opacity = parseFloat(opacitySlider.value);
+    imageLayer.setOpacity(opacity);
+    opacityLabel.innerText = opacity.toFixed(2);
+  }
 }
-opacityInput.addEventListener('input', update);
+opacitySlider.addEventListener('input', updateOpacity);
